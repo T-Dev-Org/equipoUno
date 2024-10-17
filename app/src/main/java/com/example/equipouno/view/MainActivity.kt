1bb6d6897369e791279b86c2d50bd5cafca173ea
sebastianmr18 <juan.munoz.rojas@correounivalle.edu.co>
2024-10-16 21:27:34 -0500

    refactor: cleaning MainActivity.kt

--- app/src/main/java/com/example/equipouno/view/MainActivity.kt
+++ app/src/main/java/com/example/equipouno/view/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.equipouno.view
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.equipouno.R
 
 class MainActivity : AppCompatActivity() {
