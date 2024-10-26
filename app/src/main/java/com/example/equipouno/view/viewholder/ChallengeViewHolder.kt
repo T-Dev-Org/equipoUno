d9321bb20833227302e7ca4a2f787e9f4e606783
JuanLoaiza007 <juandaloaiza723@gmail.com>
2024-10-26 17:39:12 -0500

    feat(BAPP-152_BAPP-153): implement challenge modification functionality

--- app/src/main/java/com/example/equipouno/view/viewholder/ChallengeViewHolder.kt
+++ app/src/main/java/com/example/equipouno/view/viewholder/ChallengeViewHolder.kt
@@ -7,44 +7,38 @@ import com.example.equipouno.databinding.ItemChallengeBinding
 import com.example.equipouno.model.Challenge
 import android.view.animation.AnimationUtils
 import com.example.equipouno.view.dialogue.CustomDialog
-import com.example.equipouno.view.dialogue.StandardDialog.Companion.showDialog
+import com.example.equipouno.viewmodel.ChallengeViewModel
 
 class ChallengeViewHolder(
-    binding: ItemChallengeBinding,
-    navController: NavController) :
-    RecyclerView.ViewHolder(binding.root){
+    private val binding: ItemChallengeBinding,
+    private val navController: NavController,
+    private val viewModel: ChallengeViewModel
+) : RecyclerView.ViewHolder(binding.root) {
 
-        val bindingItem = binding
+    fun setItemChallenge(challenge: Challenge) {
+        binding.tvDescription.text = challenge.description
 
-        fun setItemChallenge(challenge: Challenge) {
-            bindingItem.tvDescription.text = challenge.description
+        // Animación
+        val pressAnimation =
+            AnimationUtils.loadAnimation(binding.root.context, R.anim.scale_animation)
 
-            // Cargar la animación
-            val pressAnimation = AnimationUtils.loadAnimation(bindingItem.root.context, R.anim.scale_animation)
+        // Botón Editar
+        binding.imgbtnEdit.setOnClickListener {
+            binding.imgbtnEdit.startAnimation(pressAnimation)
 
-            // Agregar animación al pulsar el botón de editar
-            bindingItem.imgbtnEdit.setOnClickListener {
-                bindingItem.imgbtnEdit.startAnimation(pressAnimation)
-                // TODO: Crear funcionalidad de edicion
-                CustomDialog.showCustomDialog(
-                    context = bindingItem.root.context,
-                    title = "Editar Reto",
-                    hint = "Escriba el reto",
-                    positiveButtonText = "Guardar",
-                    negativeButtonText = "Cancelar",
-                    existingChallenge = challenge, // Pasar el reto existente
-                    onPositiveClick = { description ->
-                        val updatedChallenge = challenge.copy(description = description)
-                    },
-                    onNegativeClick = {}
-                )
-            }
-
-            // Agregar animación al pulsar el botón de eliminar
-            bindingItem.imgbtnDelete.setOnClickListener {
-                bindingItem.imgbtnDelete.startAnimation(pressAnimation)
-                // Acción para eliminar el reto
-                showDialog(bindingItem.root.context).show()
-            }
+            CustomDialog.showCustomDialog(
+                context = binding.root.context,
+                title = "Editar Reto",
+                hint = "Escriba el reto",
+                positiveButtonText = "Guardar",
+                negativeButtonText = "Cancelar",
+                existingChallenge = challenge,
+                onPositiveClick = { description ->
+                    val updatedChallenge = challenge.copy(description = description)
+                    viewModel.updateChallenge(updatedChallenge)
+                },
+                onNegativeClick = {}
+            )
         }
     }
+}
