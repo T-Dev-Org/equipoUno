5fbbfef4c5f091a1ff5169c20b897d1cf0b790b9
Yissy <yissyposso12@gmail.com>
2024-10-31 04:35:22 -0500

    create splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.activity)
     val navVersion = "2.3.5"
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
