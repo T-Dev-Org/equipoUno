d3cbb0e607ca2776bf9547eba048cc7d62d0b85a
juanloaiza007 <juandaloaiza723@gmail.com>
2024-11-23 18:36:50 -0500

    refactor(di): add functions to inject Repository in AppModule

--- app/src/main/java/com/example/equipouno/di/AppModule.kt
+++ app/src/main/java/com/example/equipouno/di/AppModule.kt
@@ -1,11 +1,16 @@
 package com.example.equipouno.di
 
 import com.example.equipouno.repository.ChallengeRepository
+import com.example.equipouno.repository.PokemonRepository
+import com.example.equipouno.utils.Constants.BASE_URL
+import com.example.equipouno.webservice.ApiService
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -22,5 +27,23 @@ object AppModule {
     @Singleton
     fun provideChallengeRepository(firestore: FirebaseFirestore): ChallengeRepository {
         return ChallengeRepository(firestore)
+    }
+
+    // Proporcionar ApiService
+    @Provides
+    @Singleton
+    fun provideApiService(): ApiService {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+
+    // Inyectar ApiService en PokemonRepository
+    @Provides
+    @Singleton
+    fun providePokemonRepository(apiService: ApiService): PokemonRepository {
+        return PokemonRepository(apiService)
     }
 }
